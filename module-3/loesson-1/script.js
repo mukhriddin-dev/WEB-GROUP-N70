84bca6dcb1e8f1035535467c2a47c8ee6cfb1773
mukhriddin-dev <khodievm@gmail.com>
2022-10-09 10:06:38 +0500

    lesson 1 js

---
+++ module-3/loesson-1/script.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+// document.getElementById('text').textContent="5555*123";
+// document.getElementById('box').innerHTML="<h1>"+5555*88+"</h1>";
+
+
+// console.log( document.getElementById('matn').innerHTML);
+
+//      document.write(5+25*10/100);
+//      console.log('javaScript '+" lesson 1");
+//    //   console.log(50066660*444);
+//    //   console.error("saytda xatolik");
+//    //   console.warn('internet tezligi past');
+
+//    // alert('salom')
+//    // confirm('saytdan chiqasizmi ?')
+//    // prompt('ismingiz nima ?')
+     
+  
